3bed6b92de6c97594c845fd680a806e749eceb21
Brandon Shaffer <etrudcollege@gmail.com>
2021-10-25 13:51:29 -0500

    sequelize / routes & controller backend

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
     <h1>register</h1>
+
+    <input type= "email" 
+    name = "email" 
+    v-model="email"
+    placeholder="email" />
+    <br>
+    <input type= "password" 
+    name = "password" 
+    v-model="password"
+    placeholder="password" />
+    <br>
+
+
+    <button @click="register">Register</button>
+
   </div>
 </template>
 
 <script>
+import authservice from '../services/authservice'
 export default {
   data() {
     return {
-      
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register(){
+      const response = await authservice.register({
+        email: this.email,
+        password:this.password
+      })
+      console.log(response.data)
     }
   }
-}
-
+ }
 </script>
 
 
