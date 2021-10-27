6d09a16a99518b4e66e48bbe2997090162d6395e
Brandon Shaffer <etrudcollege@gmail.com>
2021-10-27 10:43:22 -0500

    login route created / working

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,11 @@
 const authcontroller = require('../controllers/authcontroller')
-
 const authcontrollerpolicy = require('../policies/authcontrollerpolicy')
 
 module.exports = (app) => {
   app.post('/register',
     authcontrollerpolicy.register,
     authcontroller.register)
+
+  app.post('/login',
+    authcontroller.login)
 }
