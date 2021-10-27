1e8c411995515abb31926ebd83d0f17ec17cbeb4
Brandon Shaffer <etrudcollege@gmail.com>
2021-10-27 10:43:41 -0500

    updated nav bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,102 @@
 <!-- App.vue -->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-       <router-link to="/Login">Login</router-link>
-    </div>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
     <v-app>
+      <div id="nav" class="d-flex">
+      <v-app-bar dense fixed >
+        <v-toolbar-title>Grab N Go Scheduler</v-toolbar-title>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-spacer></v-spacer>
+
+        <v-card flat class="d-flex justify-end">
+          <router-link to="/Login">Login / Register</router-link>
+          <v-icon>mdi-login</v-icon>
+        </v-card>
+      </v-app-bar>
+         <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+        >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item :to="'/'">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-clock</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Punches</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-calendar</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Schedule</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-contacts-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Contact Book</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item :to="'/about'">
+            <v-list-item-icon>
+              <v-icon>mdi-cog</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item>
+
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    </div>
+    
     <router-view/>
     </v-app>
   </div>
 </template>
 
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  }
+</script>
 
 <style>
 
+
+.loginDiv {
+  float: right;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -25,7 +107,8 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px;
+  margin-bottom: 45px;
 }
 
 #nav a {
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Grab N Go Scheduler"/>
   </div>
 </template>
 
