28f00fe85761b4055085412aad75e100993a422d
Brandon <etrudcollege@gmail.com>
2021-11-05 09:39:02 -0500

    add event frontend started

--- client/src/components/addEvent.vue
+++ client/src/components/addEvent.vue
@@ -1,88 +1,55 @@
 <template>
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="600px"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          add Schedule Event
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">Add Event</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col
-                cols="15"
-                sm="14"
-                md="10"
-              >
-                <v-text-field
-                  label="Event name*"
-                  required
-                ></v-text-field>
-              </v-col>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="primary" dark v-bind="attrs" v-on="on">
+        add Schedule Event
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Add Event</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="15" sm="14" md="10">
+              <v-text-field label="Event name*" required></v-text-field>
+            </v-col>
 
-              
-              <v-col cols="12">
-                <v-datetime-picker label="Start Date / Time*" v-model="datetime"> </v-datetime-picker>
-              </v-col>
-              <v-col cols="12">
-                <v-datetime-picker label="End Date / Time*" v-model="datetime2"> </v-datetime-picker>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-select
-                  :items="users"
-                  label="Employee Assigned*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-              
+            <v-col cols="12">
+              <v-datetime-picker label="Start Date / Time*" v-model="datetime">
+              </v-datetime-picker>
+            </v-col>
+            <v-col cols="12">
+              <v-datetime-picker label="End Date / Time*" v-model="datetime2">
+              </v-datetime-picker>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select
+                :items="users"
+                label="Employee Assigned*"
+                required
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
               <v-color-picker
-  dot-size="12"
-  swatches-max-height="202"
-></v-color-picker>
-
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Add
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+                dot-size="12"
+                swatches-max-height="202"
+              ></v-color-picker>
+            </v-col>
+          </v-row>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Cancel
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="dialog = false"> Add </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -96,7 +63,10 @@ import contactservice from "@/services/contactservice";
     }),
     async mounted() {
     //do a request to a backend for all users
-    this.users = (await contactservice.index()).data
-  },
+    const fullUser = (await contactservice.index()).data
+    const updatedUser = fullUser.map(user => 'ID: '+user.id+', '+user.firstname+' '+user.lastname)
+    this.users = updatedUser
+}
   }
+  
 </script>
