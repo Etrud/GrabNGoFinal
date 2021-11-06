e2eeb79e9ad61b837e63997bcc72d42b95e82176
Brandon <etrudcollege@gmail.com>
2021-11-06 18:14:14 -0500

    fix puchall delete

--- client/src/components/punchAll.vue
+++ client/src/components/punchAll.vue
@@ -7,40 +7,35 @@
       :items-per-page="5"
       class="elevation-1"
       ><template v-slot:[`item.actions`]="{ item }">
-
         <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-
-        
-        <v-dialog v-model="dialog" persistent max-width="290">
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon small v-bind="attrs" v-on="on"> mdi-delete </v-icon>
-          </template>
-          <v-card>
-            <v-card-title class="text-h5">
-              Are you sure you want to delete this punch?
-            </v-card-title>
-            <v-card-text
-              >This will be a permanent deletion of the data.</v-card-text
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="grey darken-1" text @click="dialog = false">
-                Disagree
-              </v-btn>
-              <v-btn color="green darken-1" text @click="deleteItem(item.id)">
-                Agree
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+        <v-icon small @click="triggerDialog(item)"> mdi-delete </v-icon>
       </template>
-
       <template v-slot:[`item.punchMessage`]="{ item }">
         <v-chip :color="getColor(item.punchMessage)" dark>
           {{ item.punchMessage }}
         </v-chip>
       </template>
     </v-data-table>
+
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="text-h5">
+          Are you sure you want to delete this punch?
+        </v-card-title>
+        <v-card-text
+          >This will be a permanent deletion of the data.</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="grey darken-1" text @click="dialog = false">
+            Disagree
+          </v-btn>
+          <v-btn color="green darken-1" text @click="deleteItem(selectedItem.id)">
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -79,6 +74,10 @@ export default {
     };
   },
   methods: {
+    triggerDialog(item) {
+      this.selectedItem = item; // have to define in data section.
+      this.dialog = true; // activate dialog after assigning selected items
+    },
     getColor(punchMessage) {
       if (punchMessage == true) return "green";
       else if (punchMessage == false) return "red";
