68704a131ecff877c1bc9f92c9ab4c588f551d57
Brandon Shaffer <etrudcollege@gmail.com>
2021-11-08 09:18:50 -0600

    punch column fix

--- client/src/components/punchHistory.vue
+++ client/src/components/punchHistory.vue
@@ -1,66 +1,67 @@
 <template>
   <div>
     <v-data-table
-    dense
+      dense
       :headers="headers"
       :items="punches"
       :items-per-page="5"
       class="elevation-1"
     >
-    <template  v-slot:[`item.punchMessage`]="{ item }">
-      <v-chip
-        :color="getColor(item.punchMessage)"
-        dark
-      >
-        {{ item.punchMessage }}
-      </v-chip>
-    </template>
-    
-    
+      <template v-slot:[`item.punchMessage`]="{ item }">
+        <v-chip :color="getColor(item.punchMessage)" dark>
+          {{ item.punchMessage }}
+        </v-chip>
+      </template>
     </v-data-table>
   </div>
 </template>
 
 <script>
-import punchservice from "@/services/punchservice"
-import moment from "moment"
+import punchservice from "@/services/punchservice";
+import moment from "moment";
 
 export default {
   data() {
     return {
       punches: [],
       headers: [
-          {
-            text: 'Punch ID #',
-            align: 'start',
-            sortable: false,
-            value: 'id',
-          },
-          
-          { text: 'Punch Time', value: 'punchTime' },
-          { text: 'Punch In Message', value: 'punchMessage' },
-        ]
-      }
+        {
+          text: "Punch ID #",
+          align: "start",
+          sortable: false,
+          value: "id",
+        },
+
+        { text: "Punch Time", value: "punchTime" },
+        { text: "Punch In Message", value: "punchMessage" },
+      ],
+    };
   },
-   methods: {
-      getColor (punchMessage) {
-        if (punchMessage == true) return 'green'
-        else if (punchMessage == false) return 'red'
-        else return 'green'
-      },
+  methods: {
+    getColor(punchMessage) {
+      if (punchMessage == "CLOCK IN") return "green";
+      else if (punchMessage == "CLOCK OUT") return "red";
+      else return "green";
     },
+  },
   async mounted() {
     //do a request to a backend for all users
     const userId = this.$store.state.user.id;
-    const preChanged = (await punchservice.show(userId)).data
-        for(var i in preChanged)
-        {
-          //console.log( preChanged[i].punchTime )
-          preChanged[i].punchTime = moment(preChanged[i].punchTime).format('YYYY-MM-DD HH:mm:ss a')
-          //console.log( preChanged[i].punchTime )
-        }
-        
-        this.punches = preChanged
+    const preChanged = (await punchservice.show(userId)).data;
+    for (var i in preChanged) {
+      //console.log( preChanged[i].punchTime )
+      preChanged[i].punchTime = moment(preChanged[i].punchTime).format(
+        "YYYY-MM-DD HH:mm:ss a"
+      );
+      //console.log( preChanged[i].punchTime )
+
+      if (preChanged[i].punchMessage == true) {
+        preChanged[i].punchMessage = "CLOCK IN";
+      } else if (preChanged[i].punchMessage == false) {
+        preChanged[i].punchMessage = "CLOCK OUT";
+      }
+    }
+    this.punches = preChanged;
   },
 };
 </script>
